ced06903c904bd4375706791b13f523ae928f2d9
Samvid Dharanikota <samvid25@yahoo.com>
2017-02-22 00:04:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/page1', fuction (req, res){
+    res.send('Page 1 request will be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
