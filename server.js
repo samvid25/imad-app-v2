7cbe4340c1aae9add1e41157023a7e8b8f477ca2
Samvid Dharanikota <samvid25@yahoo.com>
2017-02-22 15:26:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,66 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var page1 = {
+    title: 'Page 1 | Hey!',
+    heading: 'Page 1 Content:',
+    content:`
+            <p>
+                Some stuff
+            </p>
+            <p>
+                Some more stuff
+            </p>
+            <p>
+                Some more more stuff
+            </p>`
+};
+
+function createTemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name = "viewport" content = "width=device-width, initial-scale = 1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class = "container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                Some stuff here:
+            </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/page1', function (req, res){
    // res.send('Page 1 request will be served here');
-    res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
+    //res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
+    res.send(createTemplate(page1));
 });
 
 app.get('/ui/style.css', function (req, res) {
